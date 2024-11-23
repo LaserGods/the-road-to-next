9addb123d20f02cc9f56c21a598901e50d10ca2b
LaserGods <jordan@hdmsarch.com>
2024-11-23 10:11:14 -0600

    add styles, tickets and ticket page, shadcn Seperator blocking dev build

---
+++ src/paths.ts
@@ -0,0 +1,5 @@
+export const homePath = () => {return '/'};
+
+export const ticketsPath = () => {return '/tickets'};
+
+export const ticketPath = (ticketId: string) => {return `/tickets/${ticketId}`};
